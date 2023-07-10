4925fe80add409b5e14e9134ee62b0b5c743be80
mkdirsx <135093670+mkdirsx@users.noreply.github.com>
2023-07-10 10:35:51 +0700

    Setup project (#1)
    * SetupProject_Done
    
    * SetupProject_Done

--- projects/client/src/App.js
+++ projects/client/src/App.js
@@ -14,6 +14,7 @@ function App() {
       setMessage(data?.message || "");
     })();
   }, []);
+  
   return (
     <div className="App">
       <header className="App-header">
---
+++ projects/server/migrations/20230704080225-create-user.js
@@ -0,0 +1,51 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      phoneNumber: {
+        type: Sequelize.STRING
+      },
+      gender: {
+        type: Sequelize.STRING
+      },
+      birthDate: {
+        type: Sequelize.DATE
+      },
+      profilePicture: {
+        type: Sequelize.STRING
+      },
+      code: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
