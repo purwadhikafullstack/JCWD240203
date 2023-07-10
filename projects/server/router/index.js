ccc5d3c8121afdae9c3c8b477ef658784cabdfa7
mkdirsx <jonathan.soetomo@gmail.com>
2023-07-10 21:27:20 +0700

    SetupProject_ResolvedConflict

--- projects/server/router/index.js
+++ projects/server/router/index.js
@@ -3,25 +3,4 @@ const { checkSchema } = require('express-validator');
 
 const Router = express.Router();
 
-Router.post('/', async(req, res, next) => {
-    await checkSchema({
-        'username': {
-            errorMessage: 'username must not be empty !',
-            notEmpty: true
-        },
-        'password': {
-            errorMessage: 'password must not be empty !',
-            notEmpty: true
-        },
-        'email': {
-            errorMessage: 'must be a valid email !',
-            isEmail: true
-        },
-        'phoneNumber': {
-            errorMessage: 'must be a valid phone number !',
-            isMobilePhone: true
-        },
-    })
-})
-
 module.exports = Router;
