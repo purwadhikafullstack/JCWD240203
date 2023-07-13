7f362b0805848ba33fb210bf8b372a988ef478f1
kazzleena07 <kareninaruth@gmail.com>
2023-07-13 22:39:57 +0700

    LandingPage

--- projects/client/src/components/LoginModal/LoginModal.jsx
+++ projects/client/src/components/LoginModal/LoginModal.jsx
@@ -24,7 +24,7 @@ export default function LoginModal(props) {
     const handleClickShowPassword = () => setShowPassword((show) => !show);
 
     const handleClose = () => {
-        if(props.setShowLogin) {
+        if (props.setShowLogin) {
             props.setShowLogin(false);
         }
         login.resetForm();
@@ -33,11 +33,11 @@ export default function LoginModal(props) {
     const validate = (value) => {
         const error = {};
 
-        if(!value.username) {
+        if (!value.username) {
             error.username = 'Required !'
         }
 
-        if(!value.password) {
+        if (!value.password) {
             error.password = 'Required !'
         }
 
@@ -59,15 +59,15 @@ export default function LoginModal(props) {
                 password: values.password
             })).then(
                 (response) => {
-                    toast.success(response.message, {id: loading});
+                    toast.success(response.message, { id: loading });
                     login.resetForm();
                 },
                 (error) => {
-                    if(!error.response.data) {
-                        toast.error('network error !', {id: loading});
+                    if (!error.response.data) {
+                        toast.error('network error !', { id: loading });
                     }
-                    else if(!Array.isArray(error.response.data.message)) {
-                        toast.error(error.response.data.message, {id: loading});
+                    else if (!Array.isArray(error.response.data.message)) {
+                        toast.error(error.response.data.message, { id: loading });
                     }
                     else {
                         toast.dismiss();
@@ -85,7 +85,7 @@ export default function LoginModal(props) {
         <div className={`${(props.showLogin)? '' : 'hidden'} flex justify-center items-center absolute top-0 w-full h-[100vh] bg-gray-400/80 z-50`}>
             <div className="relative flex flex-col items-center bg-gray-200 w-[300px] md:w-[450px] h-[400px] rounded-[10px] ">
                 <div onClick={handleClose} className="absolute flex justify-center items-center left-[20px] top-[10px] p-[5px] bg-transparent transition-all duration-400 rounded-full hover:bg-gray-300 active:animate-ping">
-                    <CloseIcon sx={{scale: '1.4'}}/>
+                    <CloseIcon sx={{ scale: '1.4' }} />
                 </div>
                 <div className="loginTitle w-full py-[10px] text-center text-[20px] font-display font-semibold text-zinc-700/90 border-b-[1px] border-gray-400">
                     Login
@@ -98,53 +98,53 @@ export default function LoginModal(props) {
                 </div>
                 <div className="w-[250px] md:w-[300px] flex flex-col gap-[20px] mb-auto mt-[10px]">
                     <div className="w-full">
-                        <TextField id="username" onChange={login.handleChange} onBlur={login.handleBlur} value={login.values.username} label='Username' size="small" fullWidth autoComplete="off"/>
+                        <TextField id="username" onChange={login.handleChange} onBlur={login.handleBlur} value={login.values.username} label='Username' size="small" fullWidth autoComplete="off" />
                         <div className="h-[5px] w-full text-[12px] text-start text-red-600">
                             {
-                                (login.touched.username && login.errors.username)?
-                                login.errors.username
-                                :
-                                <>
-                                </>
+                                (login.touched.username && login.errors.username) ?
+                                    login.errors.username
+                                    :
+                                    <>
+                                    </>
                             }
                         </div>
                     </div>
                     <div className="w-full">
-                        <FormControl sx={{width: '100%'}} variant="outlined" size="small">
+                        <FormControl sx={{ width: '100%' }} variant="outlined" size="small">
                             <InputLabel>Password</InputLabel>
                             <OutlinedInput
-                            label='Password'
-                            type={showPassword ? 'text' : 'password'}
-                            id="password"
-                            onChange={login.handleChange}
-                            onBlur={login.handleBlur}
-                            value={login.values.password}
-                            endAdornment={
-                                <InputAdornment position="end">
-                                    <IconButton 
-                                    onClick={handleClickShowPassword}
-                                    edge="end"
-                                    >
-                                        {showPassword ? <VisibilityOff /> : <Visibility />}
-                                    </IconButton>
-                                </InputAdornment>
-                            }
+                                label='Password'
+                                type={showPassword ? 'text' : 'password'}
+                                id="password"
+                                onChange={login.handleChange}
+                                onBlur={login.handleBlur}
+                                value={login.values.password}
+                                endAdornment={
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                            onClick={handleClickShowPassword}
+                                            edge="end"
+                                        >
+                                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                                        </IconButton>
+                                    </InputAdornment>
+                                }
                             />
                         </FormControl>
                         <div className="h-[5px] w-full text-[12px] text-start text-red-600">
                             {
-                                (login.touched.password && login.errors.password)?
-                                login.errors.password
-                                :
-                                <>
-                                </>
+                                (login.touched.password && login.errors.password) ?
+                                    login.errors.password
+                                    :
+                                    <>
+                                    </>
                             }
                         </div>
                     </div>
                     <div className="w-full">
-                        <Button disabled={login.isSubmitting} onClick={login.handleSubmit} variant="contained" color="success" fullWidth sx={{height: '40px'}}>
+                        <button disabled={login.isSubmitting} onClick={login.handleSubmit}  color="success" fullWidth sx={{ height: '40px' }} className="py-[8px] text-2xl font-sans rounded-[10px] bg-green-700 text-white font-extrabold cursor-pointer select-none active:scale-95 active:shadow-[0_0px_0_0_#166534,0_0px_0_0_#166534] active:border-b-[0px] transition-all duration-150 shadow-[0_10px_0_0_#166534,0_15px_0_0_] border-b-[1px] drop-shadow-xl px-10">
                             Log In
-                        </Button>
+                        </button>
                     </div>
                 </div>
             </div>
