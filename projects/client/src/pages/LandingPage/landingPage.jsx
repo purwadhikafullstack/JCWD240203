dc463d0321e618c36912fcc72d9895b786d63a8f
kazzleena07 <kareninaruth@gmail.com>
2023-07-14 14:15:01 +0700

    LandingPage

--- projects/client/src/pages/LandingPage/landingPage.jsx
+++ projects/client/src/pages/LandingPage/landingPage.jsx
@@ -107,7 +107,7 @@ export default function LandingPage() {
                     </div>
                 </div>
             </main>
-            <Footer />
+            <Footer/>
         </div >
     );
 }
