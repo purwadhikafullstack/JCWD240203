5131d6f2c50bfaac18a9b6a6dc896b2c22fd4b0f
kazzleena07 <kareninaruth@gmail.com>
2023-08-13 21:24:05 +0700

    Hosting Page

--- projects/client/src/components/LoginModal/LoginModal.jsx
+++ projects/client/src/components/LoginModal/LoginModal.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useFormik } from "formik";
 import TextField from "@mui/material/TextField";
+import ReCAPTCHA from "react-google-recaptcha";
 import OutlinedInput from '@mui/material/OutlinedInput';
 import InputLabel from '@mui/material/InputLabel';
 import InputAdornment from '@mui/material/InputAdornment';
@@ -18,6 +19,8 @@ import './LoginModal.css';
 
 export default function LoginModal(props) {
     const call = useDispatch();
+
+    const recaptchaRef = React.createRef();
 
     const [showPassword, setShowPassword] = useState(false);
 
@@ -44,111 +47,113 @@ export default function LoginModal(props) {
         return error;
     }
 
-    const login = useFormik({
-        initialValues: {
-            username: '',
-            password: '',
-        },
-        validate,
-        onSubmit: (values) => {
-            login.setSubmitting(true);
-            const loading = toast.loading('logging in !');
+    const handleSubmit = async (values) => {
+        const recaptchaValue = recaptchaRef.current.getValue();
 
-            call(onLogin({
-                username: values.username,
-                password: values.password
-            })).then(
-                (response) => {
-                    toast.success(response.message, { id: loading });
-                    login.setSubmitting(false);
-                    handleClose();
-                },
-                (error) => {
-                    if (!error.response.data) {
-                        toast.error('network error !', { id: loading });
-                    }
-                    else if (!Array.isArray(error.response.data.message)) {
-                        toast.error(error.response.data.message, { id: loading });
-                    }
-                    else {
-                        toast.dismiss();
-                        error.response.data.message.map(value => {
-                            return toast.error(value.msg);
-                        });
-                    }
-                    login.setSubmitting(false);
+        if (recaptchaValue) {
+            try {
+                const response = await call(onLogin({
+                    username: values.username,
+                    password: values.password,
+                    recaptchaToken: recaptchaValue,
+                }));
+
+                toast.success(response.message);
+                handleClose();
+            } catch (error) {
+                if (!error.response.data) {
+                    toast.error('Network error!');
+                } else if (!Array.isArray(error.response.data.message)) {
+                    toast.error(error.response.data.message);
+                } else {
+                    toast.dismiss();
+                    error.response.data.message.forEach(value => {
+                        toast.error(value.msg);
+                    });
                 }
-            )
+            }
+        } else {
+            toast.error('Please complete the reCAPTCHA.');
         }
-    })
-    
-    return(
-        <div className={`${(props.showLogin)? '' : 'hidden'} flex justify-center items-center absolute top-0 w-full h-[100vh] bg-gray-400/80 z-50`}>
-            <div className="relative flex flex-col items-center bg-gray-200 w-[300px] md:w-[450px] h-[400px] rounded-[10px] ">
-                <div onClick={handleClose} className="absolute flex justify-center items-center left-[20px] top-[10px] p-[5px] bg-transparent transition-all duration-400 rounded-full hover:bg-gray-300">
-                    <CloseIcon sx={{ scale: '1.4' }} />
+    };
+
+const login = useFormik({
+    initialValues: {
+        username: '',
+        password: '',
+    },
+    validate,
+    onSubmit: handleSubmit,
+});
+
+return (
+    <div className={`${(props.showLogin) ? '' : 'hidden'} flex justify-center items-center absolute top-0 w-full h-[100vh] bg-gray-400/80 z-50`}>
+        <div className="relative flex flex-col items-center bg-gray-200 w-[300px] md:w-[450px] h-[460px] rounded-[10px] ">
+            <div onClick={handleClose} className="absolute flex justify-center items-center left-[20px] top-[10px] p-[5px] bg-transparent transition-all duration-400 rounded-full hover:bg-gray-300">
+                <CloseIcon sx={{ scale: '1.4' }} />
+            </div>
+            <div className="loginTitle w-full py-[10px] text-center text-[20px] font-display font-semibold text-zinc-700/90 border-b-[1px] border-gray-400">
+                Login
+            </div>
+            <div className="h-[65px] w-[110px]">
+                <img alt="" src={rentifyLogo} />
+            </div>
+            <div className="welcomeBack mt-2 text-[20px] font-display font-medium ">
+                Hello, Welcome Back!
+            </div>
+            <div className="w-[250px] md:w-[300px] flex flex-col gap-[20px] mb-auto mt-[10px]">
+                <div className="w-full">
+                    <TextField id="username" onChange={login.handleChange} onBlur={login.handleBlur} value={login.values.username} label='Username' size="small" fullWidth autoComplete="off" />
+                    <div className="h-[5px] w-full text-[12px] text-start text-red-600">
+                        {
+                            (login.touched.username && login.errors.username) ?
+                                login.errors.username
+                                :
+                                <>
+                                </>
+                        }
+                    </div>
                 </div>
-                <div className="loginTitle w-full py-[10px] text-center text-[20px] font-display font-semibold text-zinc-700/90 border-b-[1px] border-gray-400">
-                    Login
-                </div>
-                <div className="h-[65px] w-[110px]">
-                    <img alt="" src={rentifyLogo} />
-                </div>
-                <div className="welcomeBack mt-2 text-[20px] font-display font-medium ">
-                   Hello, Welcome Back! 
-                </div>
-                <div className="w-[250px] md:w-[300px] flex flex-col gap-[20px] mb-auto mt-[10px]">
-                    <div className="w-full">
-                        <TextField id="username" onChange={login.handleChange} onBlur={login.handleBlur} value={login.values.username} label='Username' size="small" fullWidth autoComplete="off" />
-                        <div className="h-[5px] w-full text-[12px] text-start text-red-600">
-                            {
-                                (login.touched.username && login.errors.username) ?
-                                    login.errors.username
-                                    :
-                                    <>
-                                    </>
+                <div className="w-full">
+                    <FormControl sx={{ width: '100%' }} variant="outlined" size="small">
+                        <InputLabel>Password</InputLabel>
+                        <OutlinedInput
+                            label='Password'
+                            type={showPassword ? 'text' : 'password'}
+                            id="password"
+                            onChange={login.handleChange}
+                            onBlur={login.handleBlur}
+                            value={login.values.password}
+                            endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        onClick={handleClickShowPassword}
+                                        edge="end"
+                                    >
+                                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                                    </IconButton>
+                                </InputAdornment>
                             }
-                        </div>
+                        />
+                    </FormControl>
+                    <div className="h-[5px] w-full text-[12px] text-start text-red-600">
+                        {
+                            (login.touched.password && login.errors.password) ?
+                                login.errors.password
+                                :
+                                <>
+                                </>
+                        }
                     </div>
-                    <div className="w-full">
-                        <FormControl sx={{ width: '100%' }} variant="outlined" size="small">
-                            <InputLabel>Password</InputLabel>
-                            <OutlinedInput
-                                label='Password'
-                                type={showPassword ? 'text' : 'password'}
-                                id="password"
-                                onChange={login.handleChange}
-                                onBlur={login.handleBlur}
-                                value={login.values.password}
-                                endAdornment={
-                                    <InputAdornment position="end">
-                                        <IconButton
-                                            onClick={handleClickShowPassword}
-                                            edge="end"
-                                        >
-                                            {showPassword ? <VisibilityOff /> : <Visibility />}
-                                        </IconButton>
-                                    </InputAdornment>
-                                }
-                            />
-                        </FormControl>
-                        <div className="h-[5px] w-full text-[12px] text-start text-red-600">
-                            {
-                                (login.touched.password && login.errors.password) ?
-                                    login.errors.password
-                                    :
-                                    <>
-                                    </>
-                            }
-                        </div>
-                    </div>
-                    <div className="w-full">
-                        <button disabled={login.isSubmitting} onClick={login.handleSubmit}  color="success" sx={{ height: '40px' }} className={`py-[8px] text-2xl font-sans rounded-[10px] bg-green-700 text-white font-extrabold cursor-pointer select-none transition-all duration-150 shadow-[0_10px_0_0_#166534,0_15px_0_0_] border-b-[1px] drop-shadow-xl px-10 ${(login.isSubmitting)? 'cursor-not-allowed' : 'active:scale-95 active:shadow-[0_0px_0_0_#166534,0_0px_0_0_#166534] active:border-b-[0px]' }`}>
-                            Log In
-                        </button>
-                    </div>
+                </div>
+                <ReCAPTCHA ref={recaptchaRef} sitekey='6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI' className=" bg-gray-200"/>
+                <div className="w-full">
+                    <button disabled={login.isSubmitting} onClick={login.handleSubmit} color="success" sx={{ height: '40px' }} className={`py-[8px] text-2xl font-sans rounded-[10px] bg-green-700 text-white font-extrabold cursor-pointer select-none transition-all duration-150 shadow-[0_10px_0_0_#166534,0_15px_0_0_] border-b-[1px] drop-shadow-xl px-10 ${(login.isSubmitting) ? 'cursor-not-allowed' : 'active:scale-95 active:shadow-[0_0px_0_0_#166534,0_0px_0_0_#166534] active:border-b-[0px]'}`}>
+                        Log In
+                    </button>
                 </div>
             </div>
         </div>
-    )
+    </div>
+)
 }
